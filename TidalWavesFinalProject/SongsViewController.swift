78a5bf88d2ee2b0ab3c94db1d2d3f58ce1aab636
Riya <riya.singh45296@gmail.com>
2022-07-21 23:59:12 -0400

    Riya

--- TidalWavesFinalProject/SongsViewController.swift
+++ TidalWavesFinalProject/SongsViewController.swift
@@ -13,25 +13,31 @@ var songGenre = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
     
-    @IBAction func genreButton(_ sender: Any) {
+    @IBAction func genreButton(_ sender: UIButton) {
+        if sender.titleLabel!.text == "Pop" {
+            songGenre = "1"
+        } else if sender.titleLabel!.text == "Rock" {
+            songGenre = "2"
+        } else if sender.titleLabel!.text == "Jazz" {
+            songGenre = "3"
+        } else if sender.titleLabel!.text == "R & B / Soul" {
+            songGenre = "4"
+        } else if sender.titleLabel!.text == "Alternative / Indie" {
+            songGenre = "5"
+        } else if sender.titleLabel!.text == "Hip-Hop/ Rap" { songGenre = "6"
+        } else {
+            songGenre = "7"
+        }
+        performSegue(withIdentifier: "goToFeeling", sender: self)
     }
     
+
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+      
+
+
